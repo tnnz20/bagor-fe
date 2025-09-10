01d244fdc06fd44d5e51c85b5aabd9b56452b605
tnnz20 <kaitora200@gmail.com>
2025-09-10 17:54:08 +0800

    feat: add and set up vue router@4

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+import App from './App.vue';
+import './style.css';
 
-createApp(App).mount('#app')
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [],
+});
+
+createApp(App).use(router).mount('#app');
