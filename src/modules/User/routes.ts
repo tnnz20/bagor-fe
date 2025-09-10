8890a5d48efa871f63eabfcf0ed38e3db53f9bd0
tnnz20 <kaitora200@gmail.com>
2025-09-10 23:07:53 +0800

    refactor: move AuthLayout and update import path in routes

--- src/modules/User/routes.ts
+++ src/modules/User/routes.ts
@@ -1,6 +1,6 @@
-import AuthLayout from '@/layouts/AuthLayout.vue';
 import UserLogin from '@/modules/User/view/UserLogin.vue';
 import type { RouteRecordRaw } from 'vue-router';
+import AuthLayout from './layouts/AuthLayout.vue';
 
 export const UserRoutes: RouteRecordRaw[] = [
   {
