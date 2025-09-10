3ed2c821bd4011547e602291582f1211ba035918
tnnz20 <kaitora200@gmail.com>
2025-09-10 18:01:39 +0800

    refactor: remove HelloWorld component and set up Routerview and App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,5 @@
-<script setup lang="ts">
-import Button from './components/ui/button/Button.vue';
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <div>
-    <Button>Test</Button>
-    </div>
-   
-  
+  <RouterView />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
