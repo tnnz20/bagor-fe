a6338747ec0035a525f3d5095cd60921fa7b533c
tnnz20 <kaitora200@gmail.com>
2025-09-10 23:31:01 +0800

    refactor: remove unused attrs binding from NavigationMenu in HomeNavMenu component

--- src/modules/Home/components/HomeNavMenu.vue
+++ src/modules/Home/components/HomeNavMenu.vue
@@ -5,9 +5,6 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from '@/components/ui/navigation-menu';
-import { useAttrs } from 'vue';
-
-const attrs = useAttrs(); // forward semua atribut/props ke NavigationMenu
 
 const menuLinks = [
   { title: 'Home', href: '#' },
@@ -18,7 +15,7 @@ const menuLinks = [
 </script>
 
 <template>
-  <NavigationMenu v-bind="attrs">
+  <NavigationMenu>
     <NavigationMenuList
       class="gap-3 space-x-0 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start data-[orientation=vertical]:justify-start"
     >
