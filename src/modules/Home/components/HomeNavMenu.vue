b9cdff2ae7944fb5b193b69c6f81d9e9cc4dc52d
tnnz20 <kaitora200@gmail.com>
2025-09-10 22:30:47 +0800

    refactor: simplify HomeNavMenu component by using dynamic menu links

--- src/modules/Home/components/HomeNavMenu.vue
+++ src/modules/Home/components/HomeNavMenu.vue
@@ -4,10 +4,17 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
-} from "@/components/ui/navigation-menu"
-import { useAttrs } from "vue";
+} from '@/components/ui/navigation-menu';
+import { useAttrs } from 'vue';
 
 const attrs = useAttrs(); // forward semua atribut/props ke NavigationMenu
+
+const menuLinks = [
+  { title: 'Home', href: '#' },
+  { title: 'Blog', href: '#' },
+  { title: 'About', href: '#' },
+  { title: 'Contact Us', href: '#' },
+];
 </script>
 
 <template>
@@ -15,24 +22,9 @@ const attrs = useAttrs(); // forward semua atribut/props ke NavigationMenu
     <NavigationMenuList
       class="gap-3 space-x-0 data-[orientation=vertical]:flex-col data-[orientation=vertical]:items-start data-[orientation=vertical]:justify-start"
     >
-      <NavigationMenuItem>
+      <NavigationMenuItem v-for="(link, index) in menuLinks" :key="index">
         <NavigationMenuLink as-child>
-          <a href="#">Home</a>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink as-child>
-          <a href="#">Blog</a>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink as-child>
-          <a href="#">About</a>
-        </NavigationMenuLink>
-      </NavigationMenuItem>
-      <NavigationMenuItem>
-        <NavigationMenuLink as-child>
-          <a href="#">Contact Us</a>
+          <RouterLink :to="link.href">{{ link.title }}</RouterLink>
         </NavigationMenuLink>
       </NavigationMenuItem>
     </NavigationMenuList>
