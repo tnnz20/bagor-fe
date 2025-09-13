e681dd8a86b30263ddf4fd1b2a46c8e876974685
tnnz20 <kaitora200@gmail.com>
2025-09-14 01:53:03 +0800

    feat: implement DashboardLayout and update routing

---
+++ src/modules/Dashboard/layouts/DashboardLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <SidebarProvider>
+    <AppSidebar />
+    <SidebarInset>
+      <DashboardHeader />
+      <RouterView />
+    </SidebarInset>
+  </SidebarProvider>
+</template>
+
+<script setup lang="ts">
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import AppSidebar from '../components/AppSidebar.vue';
+import DashboardHeader from '../components/DashboardHeader.vue';
+</script>
--- src/modules/Dashboard/routes.ts
+++ src/modules/Dashboard/routes.ts
@@ -1,10 +1,12 @@
 import type { RouteRecordRaw } from 'vue-router';
 
+import DashboardLayout from './layouts/DashboardLayout.vue';
 import Dashboard from './views/Dashboard.vue';
 
 export const DashboardRoutes: RouteRecordRaw[] = [
   {
     path: '/dashboard',
+    component: DashboardLayout,
     meta: {
       title: 'Bagor - Dashboard',
       description: 'Bagor Dashboard Page',
