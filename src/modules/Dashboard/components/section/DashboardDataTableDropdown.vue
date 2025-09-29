e2bf4135caff7917ba8ece21d7b678d6f50fc19d
tnnz20 <kaitora200@gmail.com>
2025-09-29 22:59:35 +0800

    feat: create data table for dashboard section

---
+++ src/modules/Dashboard/components/section/DashboardDataTableDropdown.vue
@@ -0,0 +1,35 @@
+<template>
+  <DropdownMenu>
+    <DropdownMenuTrigger as-child>
+      <Button variant="ghost" class="h-8 w-8 p-0">
+        <span class="sr-only">Open menu</span>
+        <Icons.MoreHorizontal class="h-4 w-4" />
+      </Button>
+    </DropdownMenuTrigger>
+    <DropdownMenuContent align="end">
+      <DropdownMenuLabel>Actions</DropdownMenuLabel>
+
+      <DropdownMenuItem>
+        <RouterLink :to="`/dashboard/employees/${employee.id}`"> Lihat detail </RouterLink>
+      </DropdownMenuItem>
+    </DropdownMenuContent>
+  </DropdownMenu>
+</template>
+
+<script setup lang="ts">
+import { Icons } from '@/components/icons';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+
+defineProps<{
+  employee: {
+    id: number;
+  };
+}>();
+</script>
