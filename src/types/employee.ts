c3ac21b234d986e7cc58c25f435c6f0bf603c143
tnnz20 <kaitora200@gmail.com>
2025-10-06 21:18:05 +0800

    feat: add Department and EmployeeType types to employee.ts

--- src/types/employee.ts
+++ src/types/employee.ts
@@ -7,3 +7,18 @@ export interface Employee extends BaseModel {
   avatarUrl: string;
   score: number;
 }
+
+// Department types
+export type Department =
+  | 'UMUM DAN KEUANGAN'
+  | 'PERLENGKAPAN DAN RUMAH TANGGA'
+  | 'ORGANISASI'
+  | 'HUKUM'
+  | 'PEREKONOMIAN DAN ADMINISTRASI PEMBANGUNAN'
+  | 'PEMERINTAHAN'
+  | 'KESEJAHTERAAN RAKYAT'
+  | 'PROTOKOL DAN KOMUNIKASI PIMPINAN'
+  | 'PENGADAAN BARANG DAN JASA';
+
+// Employee types
+export type EmployeeType = 'Fungsional' | 'Struktural' | 'Kontrak';
