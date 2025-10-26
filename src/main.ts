181425adfd9ea4e2d65be174025d1e08c9b51a73
tnnz20 <kaitora200@gmail.com>
2025-10-26 21:14:24 +0800

    feat: add form shadcn, axios, and tanstackquery

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import App from './App.vue';
 
 import './assets/css/main.css';
 
+import { VueQueryPlugin } from '@tanstack/vue-query';
+
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+createApp(App).use(router).use(VueQueryPlugin).mount('#app');
