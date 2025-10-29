f4e531ee71ae5b300e45e7e80f612b1abfabf72f
Ahmad Khairol Maulidi <a.khairol.m@gmail.com>
2025-10-29 20:51:57 +0800

    feat: add title header to PegawaiView for improved context

--- src/modules/DataPegawai/views/PegawaiView.vue
+++ src/modules/DataPegawai/views/PegawaiView.vue
@@ -5,6 +5,7 @@ import DataTable from '../components/DataTable.vue';
 
 <template>
   <div class="w-full space-y-4 bg-gray-50 p-6">
+    <h1 class="text-3xl font-bold tracking-tight">Berita Acara</h1>
     <Card class="p-4">
       <DataTable />
     </Card>
