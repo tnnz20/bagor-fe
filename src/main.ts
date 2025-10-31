dd78a50dace876ffc1e3fe2ffda2af31847df121
tnnz20 <kaitora200@gmail.com>
2025-10-31 21:49:30 +0800

    feat: integrate Pinia for state management in main application

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import App from './App.vue';
 import './assets/css/main.css';
 
 import { VueQueryPlugin } from '@tanstack/vue-query';
+import { createPinia } from 'pinia';
 
 import router from './router';
 
-createApp(App).use(router).use(VueQueryPlugin).mount('#app');
+const pinia = createPinia();
+
+createApp(App).use(router).use(VueQueryPlugin).use(pinia).mount('#app');
