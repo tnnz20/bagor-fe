90cda7cf00b51b96c808ffd2dfbcc4e59b63e819
tnnz20 <kaitora200@gmail.com>
2025-11-02 00:36:11 +0800

    refactor: streamline logout button implementation in FooterSidebar component

--- src/modules/Dashboard/components/sidebar/FooterSidebar.vue
+++ src/modules/Dashboard/components/sidebar/FooterSidebar.vue
@@ -2,10 +2,7 @@
   <SidebarMenu>
     <SidebarMenuItem>
       <RouterLink to="#">
-        <Button
-          :class="cn('hover:bg-primary hover:text-accent w-full')"
-          variant="outline"
-        >
+        <Button :class="cn('hover:bg-primary hover:text-accent w-full')" variant="outline" @click="onLogout">
           <Icons.LogOut />Logout
         </Button>
       </RouterLink>
@@ -14,9 +11,21 @@
 </template>
 
 <script setup lang="ts">
+import { useRouter } from 'vue-router';
+
+import { useAuthStore } from '@/stores/auth';
+
 import { cn } from '@/lib/utils';
 
 import { Icons } from '@/components/icons';
 import { Button } from '@/components/ui/button';
 import { SidebarMenu, SidebarMenuItem } from '@/components/ui/sidebar';
+
+const router = useRouter();
+
+const authStore = useAuthStore();
+const onLogout = () => {
+  authStore.logout();
+  router.push('/login');
+};
 </script>
