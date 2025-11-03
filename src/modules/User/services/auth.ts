cccfe9cc69959b10baf2c24e4c2d924c46e1e6b0
tnnz20 <kaitora200@gmail.com>
2025-11-03 17:27:34 +0800

    refactor: rename loginUser to LoginUser for consistency and clarity

--- src/modules/User/services/auth.ts
+++ src/modules/User/services/auth.ts
@@ -1,6 +1,14 @@
 import ApiClient from '@/api/axios';
+import type { BaseApi } from '@/types';
 
-export const loginUser = async (data: { username: string; password: string }) => {
-  const res = await ApiClient.post('/auth/login', data);
+import type { UserCredential } from '@/types/user';
+
+export const LoginUser = async (userCredential: UserCredential): Promise<BaseApi> => {
+  const res = await ApiClient.post<BaseApi>('/auth/login', userCredential);
+  return res.data;
+};
+
+export const LogoutUser = async () => {
+  const res = await ApiClient.post<BaseApi>('/auth/logout');
   return res.data;
 };
