1fdf27e9ea5d2fe1359db185ce7fc2dc1938a3da
tnnz20 <kaitora200@gmail.com>
2025-11-09 08:23:52 +0800

    feat: update Question and Choice interfaces to include new properties and improve structure

--- src/types/question.ts
+++ src/types/question.ts
@@ -1,16 +1,26 @@
 import type { BaseModel } from '.';
 
+export interface QuestionCreation {
+  category: string;
+  question_text: string;
+  display_order: number;
+  choices: {
+    choice_text: string;
+    score: number;
+  }[];
+}
+
 export interface Choice extends BaseModel {
   id: number;
-  questionId: number;
-  text: string;
-  scores: number;
+  question_id: number;
+  choice_text: string;
+  score: number;
 }
 
 export interface Question extends BaseModel {
   id: number;
   category: string;
-  order: number;
-  quiz: string;
+  question_text: string;
+  display_order: number;
   choices: Choice[];
 }
