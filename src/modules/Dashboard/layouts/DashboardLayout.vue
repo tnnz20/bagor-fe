f34dcefdf8614dc08c0c0b3c79b2492da0f06da0
tnnz20 <kaitora200@gmail.com>
2025-11-30 03:43:15 +0800

    fix: increase staleTime for user query to 10 minutes

--- src/modules/Dashboard/layouts/DashboardLayout.vue
+++ src/modules/Dashboard/layouts/DashboardLayout.vue
@@ -64,7 +64,7 @@ const { data, isLoading, isError, error, isSuccess, refetch } = useQuery({
   queryKey: ['user'],
   queryFn: getUser,
   retry: 2,
-  staleTime: 5 * 60 * 1000, // 5 minutes
+  staleTime: 10 * 60 * 1000, // 5 minutes
   gcTime: 10 * 60 * 1000,
 });
 
